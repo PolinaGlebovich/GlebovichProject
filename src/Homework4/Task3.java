30b7f381deb36ee05775ccd36ae510e3a6b1d37b
glebovichh@gmail.com <glebovichh@gmail.com>
2023-04-05 16:41:08 +0300

    Homework - Arrays

--- src/Homework4/Task3.java
+++ src/Homework4/Task3.java
@@ -10,24 +10,27 @@ public class Task3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
         int n = sc.nextInt();
-        int [] arr = new int[n];
+        int[] arr = new int[n];
         float mid = 0;
 
-        int max = arr[0];
-        int min = arr[0];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(10);
 
-            if(arr[i] < min){
+            if (arr[i] < min) {
                 min = arr[i];
-            } else if(arr[i] > max){
-                max = arr[i];      // так и не поняла, как найти минимальное :/
-
+            } else if (arr[i] > max) {
+                max = arr[i];
             }
+
+            mid += arr[i] / (float)n;
         }
+
+        System.out.println(Arrays.toString(arr));
         System.out.println("Max number in array: " + max);
         System.out.println("Min number in array: " + min);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Average value: " + mid);
     }
 }
