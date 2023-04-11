be4a0286941823cde84c93434435ba9b7f7d13c6
glebovichh@gmail.com <glebovichh@gmail.com>
2023-04-11 13:40:26 +0300

    Classwork - Arrays2

--- src/homework5/Task5.java
+++ src/homework5/Task5.java
@@ -11,19 +11,15 @@ public class Task5 {
                 arr[i][j] = scanner.nextInt();
             }
         }
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                if(i == j){
-                    System.out.print(arr[i][j] + " ");
-                }
+
+            for(int j = 0; j < arr.length; j++){
+                    System.out.print(arr[j][j] + " ");
             }
-        }
         System.out.println();
+
         for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
                 System.out.print(arr[i][arr.length - i - 1] + " ");
-                break;
-            }
+
         }
     }
 }
